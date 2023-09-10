3ec7494cebc6968f3a56f6856b2dbc8e4a9214a3
jcaror99 <121507887+jcaror99@users.noreply.github.com>
2023-09-10 01:12:00 -0500

    taskTwo

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems
+  .map((element) => {
+    const newMarkup = `<li class="gallery__item">
+        <a class="gallery__link" href="${element.original}">
+            <img
+            class="gallery__image"
+            src="${element.preview}"
+            data-source="${element.original}"
+            alt="${element.description}"
+            />
+        </a>
+    </li>`;
+    return newMarkup;
+  })
+  .join("");
+gallery.innerHTML = markup;
+
+const originalImage = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
